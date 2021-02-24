b81a9256bf5a628b1ba14b55169166ceb3465c50
Manuel <carlosd99m@gmail.com>
2021-02-23 18:47:59 -0600

    Widget Apilando Textos

--- lib/review.dart
+++ lib/review.dart
@@ -1,14 +1,66 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'rating.dart';
 
 class Review extends StatelessWidget {
 
   String pathImage = "assets/img/people.jpg";
+  String name = 'Manuel Duarte';
+  String details = '1 review 5 photos';
+  double stars = 5;
+  String comment = 'There is an amazing place in Granada';
 
-  Review(this.pathImage)
+  Review(this.pathImage, this.name, this.details, this.stars, this.comment);
 
   @override
   Widget build(BuildContext context) {
+    final userComment = Container(
+      margin: EdgeInsets.only(left: 20.0),
+      child: Text(comment,
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 13.0,
+            fontWeight: FontWeight.w900,
+          )),
+    );
+
+    final userInfo = Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 20.0),
+          child: Text(details,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontFamily: 'Lato',
+                fontSize: 13.0,
+                color: Color(0xFFa3a5a7),
+              )),
+        ),
+        Rating(stars, 5.0, 14.0)
+      ],
+    );
+
+    final userName = Container(
+      margin: EdgeInsets.only(
+        left: 20.0,
+      ),
+      child: Text(name,
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 17.0,
+          )),
+    );
+
+    final userDetails = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        userName,
+        userInfo,
+        userComment,
+      ],
+    );
+
     final photo = Container(
       margin: EdgeInsets.only(
           top: 20.0,
@@ -20,8 +72,8 @@ class Review extends StatelessWidget {
       decoration: BoxDecoration(
           shape: BoxShape.circle,
           image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage(pathImage)
+              fit: BoxFit.cover,
+              image: AssetImage(pathImage)
           )
       ),
 
@@ -29,6 +81,8 @@ class Review extends StatelessWidget {
 
     return Row(
       children: [
+        photo,
+        userDetails
       ],
     );
   }
